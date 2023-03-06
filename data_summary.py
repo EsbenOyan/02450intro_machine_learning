d87cb6a8ad3e530b1118adb2d5e83bb8942cb625
Aleksander Haack <9050777+Alekhaack@users.noreply.github.com>
2023-03-06 15:56:58 +0100

    added some comments

--- data_summary.py
+++ data_summary.py
@@ -1,6 +1,7 @@
 from data_preprocessing import *
 
 def summarizeData(dfjoint: pd.DataFrame, printToConsole = True, save = False) -> pd.DataFrame:
+    # summarizes and prints/saves the data depending on function arguments
     summarizedData = dfjoint.describe()
     if save:
         summarizedData.to_csv('data_summary.csv')
@@ -9,7 +10,8 @@ def summarizeData(dfjoint: pd.DataFrame, printToConsole = True, save = False) ->
 
     return summarizedData
 
-
+# if you run this file itself this section will be run. If you import this file from another script this 
+# will not be run
 if __name__ ==  '__main__':
     dfjoint, dfRec, dfClas = dataPreprocess()
     summarizeData(dfjoint, save = True)
