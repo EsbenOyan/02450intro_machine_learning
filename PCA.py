b0ff7d0c774b0f38fc7d6bca59792804b53c73ec
jackfriskDTU <s194527@student.dtu.dk>
2023-03-06 21:42:05 +0100

    Opdatering af PCA plots

--- PCA.py
+++ PCA.py
@@ -10,51 +10,51 @@ data = pd.read_csv('./Data/Classification.data')
 mean = data.iloc[:, 2:12]
 sde = data.iloc[:, 12:22]
 worst = data.iloc[:, 22:32]
+def plot_variance(data, title):
+    # Select the columns to use in PCA
+    features = data.iloc[:, 0:10]
 
-X = mean.values
+    # Scale the features to have zero mean and unit variance
+    scaler = MinMaxScaler()
+    scaled_features = scaler.fit_transform(features)
 
-# Create a PCA object with the number of components to keep
-pca = PCA(n_components=10)
+    # Fit PCA to the scaled features
+    pca = PCA()
+    pca.fit(scaled_features)
 
-# Fit the PCA model to the data and transform the data
-X_pca = pca.fit_transform(X)
+    # Plot the explained variance ratio
+    plt.plot(range(1, pca.n_components_ + 1), pca.explained_variance_ratio_, 'bo-', linewidth=2)
+    plt.xlabel('Principal Component')
+    plt.ylabel('Explained Variance Ratio')
+    plt.title(f'{title} - Explained Variance Ratio by Principal Component')
+    plt.ylim([0,1])
+    plt.xticks(range(1, pca.n_components_ + 1))
+    plt.grid(True)
 
-# Print the explained variance ratio of each principal component
-print(pca.explained_variance_ratio_)
-print("The PC that explains the most is {} and explains {}".format(np.argmax(pca.explained_variance_ratio_), np.max(pca.explained_variance_ratio_)))
-# Create a plot showing the principal components
-plt.plot(X_pca)
-plt.title('Principal Components')
-plt.xlabel('Component')
-plt.ylabel('Variance')
-#plt.show()
+    # Plot the cumulative explained variance ratio
+    cumulative_var_ratio = np.cumsum(pca.explained_variance_ratio_)
+    plt.plot(range(1, pca.n_components_ + 1), cumulative_var_ratio, 'ro-', linewidth=2)
+    plt.xlabel('Principal Component')
+    plt.ylabel('Cumulative Explained Variance Ratio')
+    plt.title(f'{title} - Cumulative Explained Variance Ratio by Principal Component')
+    plt.ylim([0,1])
+    plt.xticks(range(1, pca.n_components_ + 1))
+    plt.grid(True)
 
-Y = sde.values 
-# Create a PCA object with the number of components to keep 
-pca = PCA(n_components=10) 
-# Fit the PCA model to the data and transform the data 
-Y_pca = pca.fit_transform(Y) 
-# Print the explained variance ratio of each principal component 
-print(pca.explained_variance_ratio_)
-print("The PC that explains the most is {} and explains {}".format(np.argmax(pca.explained_variance_ratio_), np.max(pca.explained_variance_ratio_)))
-# Create a plot showing the principal components 
-plt.plot(Y_pca) 
-plt.title('Principal Components') 
-plt.xlabel('Component') 
-plt.ylabel('Variance') 
-#plt.show()
+    # Add a dashed line at 90% cumulative explained variance ratio
+    plt.axhline(y=0.9, color='r', linestyle='--')
 
-Z = worst.values  
-# Create a PCA object with the number of components to keep 
-pca = PCA(n_components=10) 
-# Fit the PCA model to the data and transform the data 
-Z_pca = pca.fit_transform(Z) 
-# Print the explained variance ratio of each principal component 
-print(pca.explained_variance_ratio_)
-print("The PC that explains the most is {} and explains {}".format(np.argmax(pca.explained_variance_ratio_), np.max(pca.explained_variance_ratio_)))
-# Create a plot showing the principal components 
-plt.plot(Z_pca) 
-plt.title('Principal Components') 
-plt.xlabel('Component') 
-plt.ylabel('Variance') 
-#plt.show()
+    # Add legend
+    plt.legend(['Explained Variance Ratio', 'Cumulative Explained Variance Ratio', '90% Threshold'], loc='best')
+
+    plt.savefig(title + '_variance_explained.png')
+    plt.close()
+
+# Plot variance for "mean" dataset
+plot_variance(mean, 'Mean')
+
+# Plot variance for "sde" dataset
+plot_variance(sde, 'SDE')
+
+# Plot variance for "worst" dataset
+plot_variance(worst, 'Worst')
